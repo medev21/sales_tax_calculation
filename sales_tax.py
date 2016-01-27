caad3ee0b272193bb5e2195007f3eb6ae615ae2d
Martin Ben <mebenav88@gmail.com>
2016-01-26 20:23:53 -0500

    change return type to array

--- sales_tax.py
+++ sales_tax.py
@@ -2,24 +2,35 @@ class read_file(object):
     def __init__(self, file):
         self.file = file
 
-    def read(self):
+    def get_content(self):
         try:
-            # do something
-            f = open(self.file, 'r') #opens file
-            file_contents = f.read()    #read contents of file
-            print(file_contents)
-            f.close() #close the file
+            # return file contents
+            # f = open(self.file, 'r') #opens file
+            # file_contents = f.read()    #read contents of file
+            # # print(file_contents)    #print contents in the terminal
+            # f.close() #close the file
+
+            # shorter way to open and close file
+            with open(self.file) as f:
+                # file_contents = f.read()
+                file_lines = f.readlines()
+
+            return file_lines
+
         except:
-            # do something
+            # print error message
             print('something went wrong')
 
     def get_sales_tax(self):
+        line = self.get_content()
+        print(line)
+
+    # def get_total_amount(self):
         # do something
 
-    def get_total_amount(self):
-        # do something 
 
 
 tax_input1 = read_file('salesTaxInput1.txt')
 
-tax_input1.read()
+# tax_input1.get_content()
+tax_input1.get_sales_tax()
