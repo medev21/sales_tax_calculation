cbb77e5938cc305c6d0d28b4557caeeaef328c82
Martin Ben <mebenav88@gmail.com>
2016-01-26 20:59:56 -0500

    construct is_imported method

--- sales_tax.py
+++ sales_tax.py
@@ -1,19 +1,16 @@
+# books, food, medical products are exempt, other products are taxed 10%
+# imported items are taxed 5%
+
 class read_file(object):
+
     def __init__(self, file):
         self.file = file
 
     def get_content(self):
         try:
-            # return file contents
-            # f = open(self.file, 'r') #opens file
-            # file_contents = f.read()    #read contents of file
-            # # print(file_contents)    #print contents in the terminal
-            # f.close() #close the file
-
             # shorter way to open and close file
             with open(self.file) as f:
-                # file_contents = f.read()
-                file_lines = f.readlines()
+                file_lines = f.readlines() #read each line
 
             return file_lines
 
@@ -22,8 +19,34 @@ class read_file(object):
             print('something went wrong')
 
     def get_sales_tax(self):
-        line = self.get_content()
-        print(line)
+        lines = self.get_content()
+
+        # for line in lines:
+
+        print(lines)
+
+    def calculate(self):
+
+        lines = self.get_content()
+
+        for line in lines:
+            if self.is_imported(line):
+                pass
+        pass
+
+    def is_imported(self, line): #takes line(a string) as param
+        bool = False #start with false boolean
+        split_line = line.split() #split line string into an array
+
+        if any("imported" in s for s in split_line):  #check the array if contains imported
+            bool = True #set boolean to true
+        # print(bool)
+        return bool
+
+    def is_exempted(self):
+        bool = False
+        print(bool)
+        return bool
 
     # def get_total_amount(self):
         # do something
@@ -33,4 +56,4 @@ class read_file(object):
 tax_input1 = read_file('salesTaxInput1.txt')
 
 # tax_input1.get_content()
-tax_input1.get_sales_tax()
+tax_input1.is_imported("1 imported box of chocolates at 10.00")
